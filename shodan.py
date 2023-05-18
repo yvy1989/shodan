6cf6f55838a722169caf649fafc0325c0dfb65b0
yvy1989 <99916213+yvy1989@users.noreply.github.com>
2023-05-18 12:58:20 -0300

    Update shodan.py
    To obtain the open ports associated with each IP address

--- shodan.py
+++ shodan.py
@@ -1,18 +1,30 @@
 import requests
 
-api_key = input("Favor digite a sua API Key:\n")
-search_query = input("Agora insira a sua pesquisa no shodan para receber os endereços IP's:\n")
-port = input("Agora insira a porta:\n")
-
-# Send a request to the Shodan API
-#response = requests.get(f"https://api.shodan.io/shodan/host/search?key={api_key}&query={search_query}")
-response = requests.get(f"https://api.shodan.io/shodan/host/search?key={api_key}&query={search_query}&port={port}")
+API_KEY = input("Favor digite a sua API Key:\n")
+query = input("Agora insira a sua pesquisa no shodan para receber os endereços IP's:\n")
+#port = input("Agora insira a porta:\n")
 
 
-# Parse the JSON response and extract the IP addresses
+url = f"https://api.shodan.io/shodan/host/search?key={API_KEY}&query={query}"
+
+response = requests.get(url)
 data = response.json()
+
+# Extract the IP addresses from the response
 ip_addresses = [result['ip_str'] for result in data['matches']]
 
-# Print the IP addresses
+# Iterate over the IP addresses and fetch detailed information for each IP
 for ip in ip_addresses:
-    print(ip)
+    details_url = f"https://api.shodan.io/shodan/host/{ip}?key={API_KEY}"
+    details_response = requests.get(details_url)
+    details_data = details_response.json()
+
+    # Extract the open ports from the detailed information
+    open_ports = details_data.get('ports', [])
+
+    # Print the IP address and its open ports
+    print(f"IP: {ip}")
+    print("Open Ports:")
+    for port in open_ports:
+        print(port)
+    print("----------------------")
